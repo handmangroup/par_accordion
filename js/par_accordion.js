651f4eadaea4220f477f21f385c685873bed56ae
jillhandman <handmanj@si.edu>
2024-05-05 15:07:49 -0400

    initial commit

---
+++ js/par_accordion.js
@@ -0,0 +1,27 @@
+(function ($, Drupal) {
+
+    'use strict';
+    //////////////// Accordion - Click to open/collapse
+    Drupal.behaviors.Accordion = {
+        attach: function (context, settings) {
+            $('.accordion-title').click(function(){
+                var accordion = $(this).closest('.accordion');
+                var li = $(this).closest('li');
+                if (li.hasClass('active')) {
+                    li.removeClass('active');
+                } else {
+                    if (accordion.hasClass('accordion-oneopen')){
+                        var wasActive = accordion.find('li.active');
+                        wasActive.removeClass('active');
+                        (li).addClass('active');
+                    } else {
+                        li.addClass('active');
+                    }
+                }
+            });
+        }
+    };
+
+
+
+})(jQuery, Drupal);
